0696e9750d9aa8b0b1ec6383be99b20ca89997fa
refox2605 <miguel.patrick.rosales@gmail.com>
2018-01-21 15:56:54 +0100

    updated stuff

--- japrontoRun.py
+++ japrontoRun.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from japronto import Application
+import logging
 from DB import DB
+
 
 db=DB()
 def initdb(filename="data.json"):
@@ -15,86 +17,24 @@ def initdb(filename="data.json"):
     #db.printWordDict()
     return db
 
+
 def main():
-    import logging
+
     db = initdb()
     app = Application()
     r = app.router
     r.add_route('/s/{p1}/', search)
-    app.run(port=8888,debug=True)
+    app.run(port=8888)
+
 
 
 def search(request):
-    query_string=request.match_dict['p1']
-    res = db.search(query_string)
-    return request.Response(text=str())
-
-
+    try:
+        query_string=request.match_dict['p1']
+        return request.Response(text=str(db.search(query_string)))
+    except Exception as e:
+        logging.error(e, exc_info=True)
 
 if __name__ == "__main__":
     main()
 
-
-
-
-
-
-
-
-# Requests with the path set exactly to `/` and whatever method
-# will be directed here.
-# def slash(request):
-#     return request.Response(text='Hello {} /!'.format(request.method))
-#
-#
-# r.add_route('/', slash)
-#
-#
-# # Requests with the path set exactly to '/love' and the method
-# # set exactly to `GET` will be directed here.
-# def get_love(request):
-#     return request.Response(text='Got some love')
-#
-#
-# r.add_route('/love', get_love, 'GET')
-#
-#
-# # Requests with the path set exactly to '/methods' and the method
-# # set to `POST` or `DELETE` will be directed here.
-# def methods(request):
-#     return request.Response(text=request.method)
-#
-#
-# r.add_route('/methods', methods, methods=['POST', 'DELETE'])
-
-
-# Requests with the path starting with `/params/` segment and followed
-# by two additional segments will be directed here.
-# Values of the additional segments will be stored inside `request.match_dict`
-# dictionary with keys taken from {} placeholders. A request to `/params/1/2`
-# would leave `match_dict` set to `{'p1': 1, 'p2': '2'}`.
-
-
-# from tornado.web import Application, RequestHandler
-# from tornado.ioloop import IOLoop
-# from DB import DB
-#
-#     app = Application([
-#         (r"/s/(.+)", SearchHandler, dict(db=db)),
-#     ])
-#     app.listen(8888)
-#     logging.info("will listen 8888")
-#     IOLoop.current().start()
-#
-#
-#
-#
-#
-# class SearchHandler(RequestHandler):
-#     def initialize(self, db):
-#         self.db = db
-#
-#     def get(self, query_string):
-#
-#         r = self.db.search(query_string)
-#         self.write(dict(size=len(r), entries=r))
